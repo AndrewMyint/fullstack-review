4408f3654f912435dab4271a59703edeff62d940
Andrew Myint <doku.potharhtoo@gmail.com>
2019-03-08 23:16:12 -0500

    added npm start script

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const RepoList = (props) => (
-  <div>
+const RepoList = (props) => {
+
+  for (var i = 0; i < 25; i++) {
+
+  }
+
+  return (<div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
-  </div>
-)
+  </div>)
+}
 
 export default RepoList;
